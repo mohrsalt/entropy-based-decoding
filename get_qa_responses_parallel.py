ca4832744ccb51b581331546f0746b2d2134793c
Mohor Banerjee <MOHOR001@e.ntu.edu.sg>
2025-06-05 17:25:08 +0800

    device

--- get_qa_responses_parallel.py
+++ get_qa_responses_parallel.py
@@ -64,6 +64,7 @@ def main(
         with distributed_state.split_between_processes(split_prompts, apply_padding=True) as batched_prompts:
             print("Length of batch: ",len(batched_prompts))
             for id2,j in tqdm(enumerate(batched_prompts), desc=f"Generating completions on device {distributed_state.device}"):
+                print(len(j))
                 print(f"Generating completions on device inner {distributed_state.device}")    
                 
                 prompts = []
