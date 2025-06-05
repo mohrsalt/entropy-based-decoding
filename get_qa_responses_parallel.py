20aee10c940367a068566af0cc969ff0ff327d72
Mohor Banerjee <MOHOR001@e.ntu.edu.sg>
2025-06-05 18:16:58 +0800

    test

--- get_qa_responses_parallel.py
+++ get_qa_responses_parallel.py
@@ -42,7 +42,7 @@ def main(
     tokenizer.pad_token = tokenizer.eos_token
     model = AutoModelForCausalLM.from_pretrained("meta-llama/Llama-3.1-8B-Instruct",quantization_config=quantization_config, torch_dtype="auto")
     distributed_state = PartialState()
-    batch_size = 1
+    batch_size = 2
      
 
     prompt_template = "Write a high-quality answer for the given question using only the provided search results.\n\n{search_results}\n\nQuestion: {question}\nAnswer:"
@@ -64,72 +64,73 @@ def main(
         with distributed_state.split_between_processes(split_prompts, apply_padding=True) as batched_prompts:
             print("Length of batch: ",len(batched_prompts))
             for id2,jb in tqdm(enumerate(batched_prompts), desc=f"Generating completions on device {distributed_state.device}"):
-                j=jb[0]
-                print(f"Generating completions on device inner {distributed_state.device}")    
-                
-                prompts = []
-                all_model_documents = []
+                for ipo,j in enumerate(jb):
+                    
+                    print(f"Generating completions on device inner {distributed_state.device}")    
+                    
+                    prompts = []
+                    all_model_documents = []
 
-                all_model_documents_texts = []
-                questions = []
-                question=i["problem_statements"][id2]
-                matches = re.findall(pattern, j)
-                ctxs=[]
-                ttle=None
-                for ii, match in enumerate(matches, 1):
-                    if(ii!=1 and ii!=2 and ii!=4 and ii!=5 and ii!=7 and ii!=8 and ii!=10 and ii!=11):
-                        continue
-                    if(ii<4):
-                        ttle="Online Tutorials"
-                    elif(ii>3 and ii<7):
-                        ttle="Github Repos"
-                    elif(ii>6 and ii<10):
-                        ttle="Programming Solutions"
-                    else:
-                        ttle="Library Documentations"
-                    if(ii==3):
-                        m=match[1][::-1].replace(":sopeR buhtiG#", "", 1)[::-1]
-                    elif(ii==6):
-                        m=match[1][::-1].replace(":snoituloS gnimmargorP#", "", 1)[::-1]
-                    elif(ii==9):
-                        m=match[1][::-1].replace(":snoitatnemucoD yrarbiL#", "", 1)[::-1] ##Change here 
-                    else:
-                        m=match[1]
-                    ctxs.append({'title': ttle, 'text': m, 'score': None})
-                documents = []
-                for ctx in deepcopy(ctxs):
-                            documents.append(Document.from_dict(ctx))
-                prompt = get_qa_prompt(
-                            question,
-                            documents,
-                            mention_random_ordering=False,
-                            query_aware_contextualization=False,
-                        )
-                prompt = format_chat_prompt(prompt)
-                prompts.append(prompt)
-                all_model_documents.append(documents)
-                documents_texts = [f"(Title: {document.title}) {document.text}" for document in documents]
-                all_model_documents_texts.append(documents_texts)
-                questions.append(question)
-                device = distributed_state.device 
-                # change above
-                pd_ojbect = ParallelDecoding(model=model, tokenizer=tokenizer, device=device, using_norm=False,using_entropy=True)
-                
-                generate_func = pd_ojbect.clehe_using_logits
-                
-                
-                for question, documents_texts in tqdm(zip(questions, all_model_documents_texts)):
+                    all_model_documents_texts = []
+                    questions = []
+                    question=i["problem_statements"][2*id2+ipo]
+                    matches = re.findall(pattern, j)
+                    ctxs=[]
+                    ttle=None
+                    for ii, match in enumerate(matches, 1):
+                        if(ii!=1 and ii!=2 and ii!=4 and ii!=5 and ii!=7 and ii!=8 and ii!=10 and ii!=11):
+                            continue
+                        if(ii<4):
+                            ttle="Online Tutorials"
+                        elif(ii>3 and ii<7):
+                            ttle="Github Repos"
+                        elif(ii>6 and ii<10):
+                            ttle="Programming Solutions"
+                        else:
+                            ttle="Library Documentations"
+                        if(ii==3):
+                            m=match[1][::-1].replace(":sopeR buhtiG#", "", 1)[::-1]
+                        elif(ii==6):
+                            m=match[1][::-1].replace(":snoituloS gnimmargorP#", "", 1)[::-1]
+                        elif(ii==9):
+                            m=match[1][::-1].replace(":snoitatnemucoD yrarbiL#", "", 1)[::-1] ##Change here 
+                        else:
+                            m=match[1]
+                        ctxs.append({'title': ttle, 'text': m, 'score': None})
+                    documents = []
+                    for ctx in deepcopy(ctxs):
+                                documents.append(Document.from_dict(ctx))
+                    prompt = get_qa_prompt(
+                                question,
+                                documents,
+                                mention_random_ordering=False,
+                                query_aware_contextualization=False,
+                            )
+                    prompt = format_chat_prompt(prompt)
+                    prompts.append(prompt)
+                    all_model_documents.append(documents)
+                    documents_texts = [f"(Title: {document.title}) {document.text}" for document in documents]
+                    all_model_documents_texts.append(documents_texts)
+                    questions.append(question)
+                    device = distributed_state.device 
+                    # change above
+                    pd_ojbect = ParallelDecoding(model=model, tokenizer=tokenizer, device=device, using_norm=False,using_entropy=True)
+                    
+                    generate_func = pd_ojbect.clehe_using_logits
+                    
+                    
+                    for question, documents_texts in tqdm(zip(questions, all_model_documents_texts)):
 
-                        print("Response here: ")
-                        _, _, response, _ = generate_func(prompt_template=prompt_template, prompt_template_wo_results=prompt_template_wo_results,
-                                            question=question, document_texts=documents_texts, max_tokens=800,
-                                            beta=0.25, temp_cpmi=0.1, metric_criterion="weighted_entropy", temperature=1e-4,
-                                            sampling_method="greedy", alpha=0.1,
-                                            candidate_layers=[2,4,6,8,10,12,14,16,18,20,22,24,26,28,30,32],
-                                            reweight_logit=False)
-                        print(response)
-                        kldout.append(response)
-                torch.cuda.empty_cache()
+                            print("Response here: ")
+                            _, _, response, _ = generate_func(prompt_template=prompt_template, prompt_template_wo_results=prompt_template_wo_results,
+                                                question=question, document_texts=documents_texts, max_tokens=800,
+                                                beta=0.25, temp_cpmi=0.1, metric_criterion="weighted_entropy", temperature=1e-4,
+                                                sampling_method="greedy", alpha=0.1,
+                                                candidate_layers=[2,4,6,8,10,12,14,16,18,20,22,24,26,28,30,32],
+                                                reweight_logit=False)
+                            print(response)
+                            kldout.append(response)
+                    torch.cuda.empty_cache()
         i["outputs"]=gather_object(kldout)
     if distributed_state.is_main_process:
     
